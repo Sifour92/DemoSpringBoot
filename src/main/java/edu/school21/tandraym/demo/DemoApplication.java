77599a4cafe07594201ee0c23cb507498932f3b6
Aleksandr Bernatovich <sagder91@gmail.com>
2023-12-08 12:20:32 +0300

    develop add task1

---
+++ src/main/java/edu/school21/tandraym/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package edu.school21.tandraym.demo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@AllArgsConstructor
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/edu/school21/tandraym/demo/model/Role.java
@@ -0,0 +1,6 @@
+package edu.school21.tandraym.demo.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
